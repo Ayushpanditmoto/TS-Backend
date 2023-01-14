e83a0620f65f4800ef88521941b1b6db34f6f1e9
Ayush Kumar Pandit <ayushpanditmoto@gmail.com>
2023-01-14 08:06:36 +0530

    feat : crud opertion woth typescript done

--- src/Controllers/crudControllers.ts
+++ src/Controllers/crudControllers.ts
@@ -1,33 +1,106 @@
 import { Request, Response } from "express";
+import Todo from "../Models/todoModels";
+
 const getAllUsers= async (req: Request, res: Response) => {
+    try {
+        const todos = await Todo.find();
     res.status(200).json({
-        message: "Get all users"
+        message: "Get all users",
+        todos
     });
+        
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({
+            message: "Something went wrong",
+            error
+        });
+        
+    }
+    
     
 }
 const getUser= async (req: Request, res: Response) => {
+    try {
+        
+    const todo = await Todo.findById(req.params.id);
     res.status(200).json({
-        message: "Get user"
+        message: "Get user",
+        todo
     });
+        
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({
+            message: "Something went wrong",
+            error
+        });
+        
+    }
     
 }
 const createUser= async (req: Request, res: Response) => {
-    res.status(200).json({
-        message: "Create user"
+    try {
+        const {title,description} = req.body;
+    const todo = await Todo.create({
+        title,
+        description
     });
+    res.status(200).json({
+        message: "Create user",
+        todo
+    });
+        
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({
+            message: "Something went wrong",
+            error
+        });
+        
+    }
     
 }
 
 const updateUser= async (req: Request, res: Response) => {
+    try {
+        const {id} = req.params;
+    const {title,description} = req.body;
+    const todo = await Todo.findByIdAndUpdate(id,{
+        title,
+        description
+    },{new:true});
     res.status(200).json({
-        message: "Update user"
+        message: "Update user",
+        todo
     });
-    
+        
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({
+            message: "Something went wrong",
+            error
+        });
+        
+    }    
 }
 const deleteUser= async (req: Request, res: Response) => {
+    try {
+        const {id} = req.params;
+    const todo = await Todo.findByIdAndDelete(id);
     res.status(200).json({
-        message: "Delete user"
+        message: "Delete user",
+        todo
     });
+        
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({
+            message: "Something went wrong",
+            error
+        }); 
+        
+    }
     
 }
 
